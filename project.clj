3f728f46fa7f614bd3ab470e4fc00ee7b02407a5
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-07 12:42:19 +0100

    Task 40: User create/update duplication key exception messages implemented.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.4"
+(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.5"
   :description "Validator library"
   :url "http://github.com/VladimirMarkovic86/validator-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.3"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.8"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.9"]
                  ]
 
   :min-lein-version "2.0.0"
