b5b5f81dd56e1703f243fcca84b2a4450dd2cabb
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-15 11:03:51 +0100

    Task 54: Fixed po and ocr

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.6"
+(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.7"
   :description "Validator library"
   :url "http://github.com/VladimirMarkovic86/validator-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.4"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.10"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.5"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.11"]
                  ]
 
   :min-lein-version "2.0.0"
