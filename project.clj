3b25f0ba4982d071b0fbfb5ff549951921af7b51
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-21 13:00:39 +0100

    Task 63: Implemented tests for dao-lib.core namespace.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.13"
+(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.14"
   :description "Validator library"
   :url "http://github.com/VladimirMarkovic86/validator-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.8"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.17"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.18"]
                  ]
 
   :min-lein-version "2.0.0"
