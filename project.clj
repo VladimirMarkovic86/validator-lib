402b59d02cf4d4d73ac022ddde238b9ad53ec696
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-04-04 18:00:49 +0200

    Task63: Implemented server-lib.core namespace tests and fixed some bugs.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.17"
+(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.18"
   :description "Validator library"
   :url "http://github.com/VladimirMarkovic86/validator-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.8"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.21"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.9"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.22"]
                  ]
 
   :min-lein-version "2.0.0"
