66cbfb0c04bc7394dc6e37ee44fe865ab2e7665c
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-03 17:08:53 +0200

    Task 83: Refactored and fixed session/long-session bug.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.25"
+(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.26"
   :description "Validator library"
   :url "http://github.com/VladimirMarkovic86/validator-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.13"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.27"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.14"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.28"]
                  ]
 
   :min-lein-version "2.0.0"
