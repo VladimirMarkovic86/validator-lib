9e43c466672fbaefa0e63bfd7f98262bc2f9ba41
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-14 14:29:22 +0200

    Task 64: Implemented card view in html and latex reports, preferences window is available also.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.27"
+(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.28"
   :description "Validator library"
   :url "http://github.com/VladimirMarkovic86/validator-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.15"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.29"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.30"]
                  ]
 
   :min-lein-version "2.0.0"
