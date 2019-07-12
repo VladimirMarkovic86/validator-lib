43b5595dbf9d17f821e6b67eea16583a9c1b654d
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-12 15:50:49 +0200

    Task 88: Implemented droid-server project, it is as library to be used with AndroidServer android java project.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.32"
+(defproject org.clojars.vladimirmarkovic86/validator-lib "0.1.33"
   :description "Validator library"
   :url "http://github.com/VladimirMarkovic86/validator-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.16"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.31"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.32"]
                  ]
 
   :min-lein-version "2.0.0"
